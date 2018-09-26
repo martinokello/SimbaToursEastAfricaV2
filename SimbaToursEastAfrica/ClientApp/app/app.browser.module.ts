b25d6a20c93c164138a35bbeacdfe6e499671fc3
EPSILONX\martinokello <martin.okello@gmail.com>
2018-09-26 01:33:35 +0100

    2nd commit troubleshooting AppModule not NgModule

--- SimbaToursEastAfrica/ClientApp/app/app.browser.module.ts
+++ SimbaToursEastAfrica/ClientApp/app/app.browser.module.ts
@@ -1,13 +1,21 @@
 ﻿import { NgModule } from '@angular/core';
 import { AppModule } from './app.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from '../app.component';
 
 
 @NgModule({
     imports: [
         AppModule,
-        BrowserModule
+        BrowserModule,
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule
     ],
     providers: [
         { provide: 'BASE_URL', useFactory: getBaseUrl }
--- SimbaToursEastAfrica/ClientApp/app/app.server.module.ts
+++ SimbaToursEastAfrica/ClientApp/app/app.server.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AppModule } from './app.module';
 import { ServerModule } from '@angular/platform-server';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from '../app.component';
 @NgModule({
     imports: [
+        ServerModule,
+        CommonModule,
+        HttpModule,
+        FormsModule,
         AppModule,
-        ServerModule
+        RouterModule
     ],
     bootstrap: [AppComponent]
 })
