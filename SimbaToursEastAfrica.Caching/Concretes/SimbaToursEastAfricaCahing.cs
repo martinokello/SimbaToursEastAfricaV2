917f4ccd2fded6ad9c5d9b3e875f94bef3babed6
Martin Alex Okello <martin.okello@gmail.com>
2019-12-11 07:06:03 +0000

    Update to Simba Tours Caching resolution
    Update to Simba Tours Caching resolution

--- SimbaToursEastAfrica.Caching/Concretes/SimbaToursEastAfricaCahing.cs
+++ SimbaToursEastAfrica.Caching/Concretes/SimbaToursEastAfricaCahing.cs
@@ -14,14 +14,14 @@ namespace SimbaToursEastAfrica.Caching.Concretes
 
         public T GetOrSaveToCache<T>(T cachedObject,string key, int timeInMinutes, Func<T> ResolveCache)
         {
-            var result = CacheObject.Get(key);
+            T result = CacheObject.Get(key);
             if (object.Equals(result, default(T)))
             {
                 T fromCache = ResolveCache.Invoke();
                 CacheObject.Set(key, fromCache, DateTime.Now.AddMinutes(timeInMinutes));
                 return fromCache;
             }
-            return default(T); 
+            return result; 
         }
     }
 }
